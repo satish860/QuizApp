f3bf9370f1ebdd1ed6639213d666029c82c24ab1
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-30 11:04:05 +0530

    Total Score done

--- Pages/Quiz.js
+++ Pages/Quiz.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Pressable, StyleSheet, Text, View,ActivityIndicator } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
 
 function Quiz() {
   const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [questionId, setQuestionId] = useState(0);
+  const [totalScore, settotalScore] = useState(0);
   const getQuestions = async () => {
     try {
       const response = await fetch(
@@ -18,44 +26,76 @@ function Quiz() {
     }
   };
 
+  const onOptionPress = (answer, optionId) => {
+    if (answer == optionId) {
+      settotalScore(totalScore + 10);
+    }
+    OnNextEvent();
+  };
+
+  const OnNextEvent = () => {
+    var nextQuestionId = questionId + 1;
+    setQuestionId(nextQuestionId);
+  };
+
   useEffect(() => {
     getQuestions();
   }, []);
 
   return (
     <View style={styles.container}>
-    {isLoading ? (
-      <ActivityIndicator />
-    ) : (
-      <>
-        <View style={styles.questionContainer}>
-          <Text style={styles.question}>{data[0].Questions}</Text>
-        </View>
-        <View style={styles.answerContainer}>
-          <Pressable style={styles.buttonStyle}>
-            <Text>{data[0]["Option-1"]}</Text>
-          </Pressable>
-          <Pressable style={styles.buttonStyle}>
-            <Text>{data[0]["Option-2"]}</Text>
-          </Pressable>
-          <Pressable style={styles.buttonStyle}>
-            <Text>{data[0]["Option-3"]}</Text>
-          </Pressable>
-          <Pressable style={styles.buttonStyle}>
-            <Text>{data[0]["Option-4"]}</Text>
-          </Pressable>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Pressable style={styles.buttonStyle}>
-            <Text>Back</Text>
-          </Pressable>
-          <Pressable style={styles.buttonStyle}>
-            <Text>Next</Text>
-          </Pressable>
-        </View>
-      </>
-    )}
-  </View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <View style={styles.questionContainer}>
+            <Text style={styles.question}>{data[questionId].Questions}</Text>
+          </View>
+          <View style={styles.answerContainer}>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => {
+                onOptionPress(data[questionId]["Answer"], 1);
+              }}
+            >
+              <Text>{data[questionId]["Option-1"]}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => {
+                onOptionPress(data[questionId]["Answer"], 2);
+              }}
+            >
+              <Text>{data[questionId]["Option-2"]}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => {
+                onOptionPress(data[questionId]["Answer"], 3);
+              }}
+            >
+              <Text>{data[questionId]["Option-3"]}</Text>
+            </Pressable>
+            <Pressable
+              style={styles.buttonStyle}
+              onPress={() => {
+                onOptionPress(data[questionId]["Answer"], 4);
+              }}
+            >
+              <Text>{data[questionId]["Option-4"]}</Text>
+            </Pressable>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.buttonStyle}>
+              <Text>Back</Text>
+            </Pressable>
+            <Pressable style={styles.buttonStyle} onPress={() => OnNextEvent()}>
+              <Text>Next</Text>
+            </Pressable>
+          </View>
+        </>
+      )}
+    </View>
   );
 }
 
